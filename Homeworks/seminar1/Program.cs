a1682909ec432e2ec8013ea61c03388fe2171fc2
Darya <vdv.ex772237.ru@gmail.com>
2022-10-20 12:37:54 +0400

    seminar 1 dz

---
+++ Homeworks/seminar1/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+/*
+Console.WriteLine("Введите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.WriteLine("Первое число " + num1 + " больше чем второе " + num2);
+}
+else
+{
+    Console.WriteLine("Второе число " + num2 + " больше чем первое " + num1);
+}
+*/
+
+// Задача 4:Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+/*
+Console.WriteLine("Введите 3 числа: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+int max = num1;
+if (num2 > max)
+{
+    max = num2;
+}
+if (num3 > max)
+{
+    max = num3;
+}
+Console.WriteLine("Наибольшее из введенных чисел -> " + max);
+*/
+
+// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+/*
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 1)
+{
+    Console.WriteLine("Число " + num + " является нечетным");
+}
+else
+{
+    Console.WriteLine("Число " + num + " является четным");
+}
+*/
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int current = 1;
+bool not = true;
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Четные числа от 1 до " + num);
+while (current <= num)
+{
+    if (current % 2 != 1)
+    {
+        Console.Write(current + " ");
+        not = false;
+    }
+    current++;
+}
+if (not)
+{
+    Console.WriteLine("Нет четных чисел!");
+}
