b130e5bd591337627a63cc9ade6bffc36e05c66d
Darya <vdv.ex772237.ru@gmail.com>
2022-10-22 11:58:09 +0400

    seminar 3 dz

--- Homeworks/seminar3/Program.cs
+++ Homeworks/seminar3/Program.cs
@@ -23,7 +23,7 @@ else
 
 // Задача 21. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 /*
-double FindWay (double xa, double xb, double ya, double yb, double za, double zb)
+double Decision (double xa, double xb, double ya, double yb, double za, double zb)
 {
     return Math.Sqrt(Math.Pow(xb - xa, 2) + Math.Pow(yb - ya, 2) + Math.Pow(zb - za, 2));
 }
@@ -41,13 +41,13 @@ double za = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Input zb: ");
 double zb = Convert.ToDouble(Console.ReadLine());
 
-double findWay = FindWay(xa, xb, ya, yb, za, zb);
+double segmentLength =  Math.Round (Decision(xa, xb, ya, yb, za, zb), 2 );
 
-Console.WriteLine($"Way = {findWay}");
+Console.WriteLine($"Длина отрезка  {segmentLength}");
 */
 
 // Задача 23. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-
+/*
 Console.Write("Введите число N: ");
 int num = Convert.ToInt32(Console.ReadLine());
 for (int i = 1; i <= num; i++) 
@@ -55,3 +55,4 @@ for (int i = 1; i <= num; i++)
     Console.Write($"{Math.Pow(i, 3)}, ");
 }
 Console.WriteLine(" ");
+*/
